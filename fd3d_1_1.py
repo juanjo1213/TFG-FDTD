57c7ea659d21a31fc5992143c4b6e8a46d659d1d
juanjo1213 <juanjorafaangel@gmail.com>
2022-05-16 11:23:41 +0200

    Se modifica el algoritmo fd3d_1_1 para mostrar los valores del pulso por pantalla

--- fd3d_1_1.py
+++ fd3d_1_1.py
@@ -11,18 +11,22 @@ hy = np.zeros(ke)
 kc = int(ke / 2)
 t0 = 40
 spread = 12
-nsteps = 100
+nsteps = 200
 # Main FDTD Loop
 for time_step in range(1, nsteps + 1):
      # Calculate the Ex field
      for k in range(1, ke):
-          ex[k] = ex[k] + 0.5 * (hy[k - 1] - hy[k])
+          ex[k] = ex[k] + 0.5 * (hy[k - 1] - hy[k]) 
      # Put a Gaussian pulse in the middle
      pulse = exp(-0.5 * ((t0 - time_step) / spread) ** 2)
      ex[kc] = pulse
+     #print(pulse)
      # Calculate the Hy field
      for k in range(ke - 1):
           hy[k] = hy[k] + 0.5 * (ex[k] - ex[k + 1])
+#Save vectors in a .txt file
+
+
 # Plot the outputs as shown in Fig. 1.2
 plt.rcParams['font.size'] = 12
 plt.figure(figsize=(8, 3.5))
@@ -45,3 +49,7 @@ plt.yticks(np.arange(-1, 1.2, step=1))
 plt.ylim(-1.2, 1.2)
 plt.subplots_adjust(bottom=0.2, hspace=0.45)
 plt.show()
+
+
+
+#print(ex)
---
+++ learning_py/Bucles.py
@@ -0,0 +1,10 @@
+for i in range(11):
+    print(i)
+
+
+a=1
+while a<10:
+    print("!Hola mundo!")
+    a=a+1
+
+
