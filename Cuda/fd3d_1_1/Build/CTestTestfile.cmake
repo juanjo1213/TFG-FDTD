6faecd017fd7964c63209356ac4fe7f8aabdff7f
Luis Manuel Diaz Angulo <LM@DiazAngulo.com>
2022-05-24 13:16:14 +0200

    Reorganizes code

--- Cuda/fd3d_1_1/Build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/jojus/Desktop/TFG/Projects/Cuda/fd3d_1_1/Code
-# Build directory: C:/Users/jojus/Desktop/TFG/Projects/Cuda/fd3d_1_1/Build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
---
+++ src/fd1d_2_1.cu
@@ -0,0 +1,186 @@
+//Calculus of the electromagnetic field using the FDTD method in Cuda with boundary condition so we have no reflected wave.
+// In this case it is set a dielectric medium at the half right of the space, with epsilon=4.
+// We use a sinusoidal function as pulse
+// It is considered a lossy dielectric medium
+// Now we generalize the method including the vector D
+//Juan Jose Salazar
+
+//Libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <math.h>       /* sin */
+
+#include <cuda_runtime.h>
+#include "fdtd.h"
+
+#define PI 3.14159265358979323
+
+using namespace std;
+
+//Electromagnetic vectors dimension
+#define dim 200
+
+struct FDTDData {
+	double* e;
+	double* h;
+	double* aux;
+	double* gax;
+	double* gbx;
+	double* dx;
+	double* idx;
+}
+
+void allocateOnHost(FDTDData& host, std::size_t dim)
+{
+	host.e = (double*)malloc(dim * sizeof(double));
+	host.h = (double*)malloc(dim * sizeof(double));
+	host.aux = (double*)malloc(2 * sizeof(double));
+	host.gax = (double*)malloc(dim * sizeof(double));
+	host.gbx = (double*)malloc(dim * sizeof(double));
+	host.dx = (double*)malloc(dim * sizeof(double));
+	host.ix = (double*)malloc(dim * sizeof(double));
+}
+
+void allocateOnDevice(FDTDData& dev)
+{
+	cudaMalloc(dev.e, dim * sizeof(double));
+	cudaMalloc(dev.h, dim * sizeof(double));
+	cudaMalloc(dev.aux, 2 * sizeof(double));
+	cudaMalloc(dev.gax, dim * sizeof(double));
+	cudaMalloc(dev.gbx, dim * sizeof(double));
+	cudaMalloc(dev.dx, dim * sizeof(double));
+	cudaMalloc(dev.ix, dim * sizeof(double));
+}
+
+//Main function
+int main(int argc, char** argv)
+{
+	
+	//Block declaration
+	int bloques = 1;
+
+	//Pulse parameter declaration
+	double pulse;
+	int kc = (int)dim / 2;
+	double ddx = 0.01;        //Cell size
+	double dt = ddx / 6e8;    //Time step size
+	double freq_in = 700e6;
+
+	//Dielectric Profile
+	double epsz = 8.854e-12;
+	double epsr = 4;
+	double epsilon = 4;
+	double sigma = 0.04;
+	double eaf = dt * sigma / (2 * epsz * epsilon);
+
+	//Number of steps done
+	int nsteps = 500;
+
+	
+	FDTDData hst, dev;
+	allocateOnHost(hst);
+	allocateOnDevice(dev);
+
+	//Vectors initialization
+	for (int i = 0;i < dim;i++)
+	{
+		hst.e[i] = 0;
+		hst.h[i] = 0;
+		hst.dx[i] = 0;
+		hst.ix[i] = 0;
+
+		//Inicializo epsilon
+		if (i < kc)
+		{
+			hst_gax[i] = 1;
+			hst_gbx[i] = 0;
+		}
+		else
+		{
+			hst_gax[i] = 1 / (epsr + (sigma * dt / epsz));
+			hst_gbx[i] = sigma * dt / epsz;
+
+		}
+		//cout << hst_gbx[i] << endl;
+	}
+
+	//Auxiliar vector for boundary conditions initialization
+	hst_aux[0] = 0;
+	hst_aux[1] = 0;
+
+	//Host to device information movement
+	cudaMemcpy(dev_e, hst_e, dim * sizeof(double), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_h, hst_h, dim * sizeof(double), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_aux, hst_aux, 2 * sizeof(double), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_gax, hst_gax, dim * sizeof(double), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_gbx, hst_gbx, dim * sizeof(double), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_dx, hst_dx, dim * sizeof(double), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_ix, hst_ix, dim * sizeof(double), cudaMemcpyHostToDevice);
+
+	for (int i = 1;i < nsteps + 1;i++)
+	{
+		//Pulse generation
+		pulse = sin(2 * PI * freq_in * dt * i);
+		//cout << pulse << endl;
+		//Kernel function call to use 200 wires (vectors dimension) to calculate the fields
+		fdtd << <bloques, dim >> > (dev_e, dev_h, dev_aux, dev_gax, dev_gbx,dev_dx,dev_ix, dim, pulse);
+	}
+
+
+
+	//Device to host information movement
+	cudaMemcpy(hst_e, dev_e, dim * sizeof(double), cudaMemcpyDeviceToHost);
+	cudaMemcpy(hst_h, dev_h, dim * sizeof(double), cudaMemcpyDeviceToHost);
+	cudaMemcpy(hst_aux, dev_aux, 2 * sizeof(double), cudaMemcpyDeviceToHost);
+	cudaMemcpy(hst_gax, dev_gax, dim * sizeof(double), cudaMemcpyDeviceToHost);
+	cudaMemcpy(hst_gbx, dev_gbx, dim * sizeof(double), cudaMemcpyDeviceToHost);
+	cudaMemcpy(hst_dx, dev_dx, dim * sizeof(double), cudaMemcpyDeviceToHost);
+	cudaMemcpy(hst_ix, dev_ix, dim * sizeof(double), cudaMemcpyDeviceToHost);
+
+
+	//Output files declaration (each one to store the fields values)
+	ofstream ex;
+	ofstream hy;
+
+	//File opening
+	ex.open("ex.txt");
+	hy.open("hy.txt");
+
+
+	//Results writing 
+	for (int i = 0;i < dim;i++)
+	{
+		ex << hst_e[i] << endl;
+		hy << hst_h[i] << endl;
+		//cout << hst_e[i] << endl;
+	}
+
+
+	// Device and host memory release
+	cudaFree(dev_e);
+	cudaFree(dev_h);
+	cudaFree(dev_aux);
+	cudaFree(dev_gax);
+	cudaFree(dev_gbx);
+	cudaFree(dev_dx);
+	cudaFree(dev_ix);
+
+	free(hst_e);
+	free(hst_h);
+	free(hst_aux);
+	free(hst_gax);
+	free(hst_gbx);
+	free(hst_dx);
+	free(hst_ix);
+
+	//File closing
+	ex.close();
+	hy.close();
+
+
+	return 0;
+}
+
+
