211a49248e54e1274d6428ef6b73c8c29a97a6c5
juanjo1213 <juanjorafaangel@gmail.com>
2022-06-05 23:48:31 +0200

    Solution using kernel and host

--- src/fd2d.cu
+++ src/fd2d.cu
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <math.h>
 #include <iomanip>
+#include <fstream>
 
 #include <cuda_runtime.h>
 
@@ -15,38 +16,40 @@ using namespace std;
 typedef struct
 {
 	double ez,
-		   dz,
-		   hx,
-		   hy,
-		   iz,
-		   ihx,
-		   ihy,
-		   gaz,
-		   gbz,
-		   curl_e;
+		dz,
+		hx,
+		hy,
+		iz,
+		ihx,
+		ihy,
+		gaz,
+		gbz,
+		curl_e,
+		aux;  //auxiliar matrix for calculations
 
 }FDTDData;
 
 typedef struct
 {
 	double gi2,
-		   gi3,
-		   fi1,
-		   fi2,
-		   fi3,
-		   
-		   gj2,
-		   gj3,
-		   fj1,
-		   fj2,
-		   fj3;
+		gi3,
+		fi1,
+		fi2,
+		fi3,
+
+		gj2,
+		gj3,
+		fj1,
+		fj2,
+		fj3;
 }PML;
 
 
 typedef struct
 {
 	double ez_inc,
-		   hx_inc;
+		aux2, //auxiliar vector for calculus
+		hx_inc;
 }INCFIELD;
 
 void ini(FDTDData* data, INCFIELD* field, int xdim, int ydim)
@@ -54,17 +57,19 @@ void ini(FDTDData* data, INCFIELD* field, int xdim, int ydim)
 	for (int i = 0;i < xdim; i++) {
 		for (int j = 0;j < ydim;j++) {
 
-			    data[j * xdim + i].ez  = 0.0; 
-				data[j * xdim + i].dz = 0.0;
-				data[j * xdim + i].hx  = 0.0; 
-				data[j * xdim + i].hy  = 0.0; 
-				data[j * xdim + i].iz  = 0.0; 
-				data[j * xdim + i].ihx = 0.0; 
-				data[j * xdim + i].ihy = 0.0; 
-				data[j * xdim + i].curl_e = 0.0;
+			data[j * xdim + i].ez = 0.0;
+			data[j * xdim + i].dz = 0.0;
+			data[j * xdim + i].hx = 0.0;
+			data[j * xdim + i].hy = 0.0;
+			data[j * xdim + i].iz = 0.0;
+			data[j * xdim + i].ihx = 0.0;
+			data[j * xdim + i].ihy = 0.0;
+			data[j * xdim + i].curl_e = 0.0;
+			data[j * xdim + i].aux = 0.0;
 		}
 		field[i].ez_inc = 0.0;
 		field[i].hx_inc = 0.0;
+		field[i].aux2 = 0.0;
 		//cout << field[i].ez_inc  << ",  ";
 	}
 
@@ -96,11 +101,11 @@ void PMLGEN(PML* parameter, int x, int y)
 
 	}
 
-	for (int n=0; n < npml;n++)
+	for (int n = 0; n < npml;n++)
 	{
 		int xnum = npml - n;
 		double xd = npml;
-		double xxn =(double) xnum / xd;
+		double xxn = (double)xnum / xd;
 		double xn = 0.33 * xxn * xxn * xxn;
 
 		parameter[n].gi2 = 1 / (1 + xn);
@@ -169,112 +174,167 @@ void cilinder(FDTDData* data, int xdim, int ydim)
 	}
 }
 
-__global__ void FDTD(FDTDData* data, PML* pml, INCFIELD* field, int xdim, int ydim)
+__global__ void FDTD(FDTDData* data, PML* pml, INCFIELD* field, int xdim, int ydim, int time_step)
 {
-	
+
 	int fil = blockIdx.y * blockDim.y + threadIdx.y;
 	int col = blockIdx.x * blockDim.x + threadIdx.x;
 	int ia = 7,
 		ib = xdim - ia - 1,
 		ja = 7,
-		jb = xdim - ja - 1;
+		jb = ydim - ja - 1;
 
-	//Boundary Conditions parameters
-	double low1 = 0.0,
-		   low2 = 0.0,
-		   high1 = 0.0,
-		   high2 = 0.0;
 	//Pulse parameters and steps
 	double pulse,
-		   t0 = 20.0,
-		   spread = 8.0;
-	int nsteps = 100;
+		t0 = 25.0,
+		spread = 8.0;
 
-	
-	for (int time_step = 1;time_step < nsteps + 1;time_step++)
-	{
-		//Incident Ez values
-		if (0 < fil && fil<ydim) {
-			field[fil].ez_inc = field[fil].ez_inc + 0.5 * (field[fil - 1].hx_inc - field[fil].hx_inc);
-		}
 
-		//Absorbing Boundary Conditions
-		field[0].ez_inc = low1;
-		low1 = low2;
-		low2 = field[1].ez_inc;
 
-		field[ydim - 1].ez_inc = high1;
-		high1 = high2;
-		high2 = field[ydim - 2].ez_inc;
-		
-		//Calculate the Dz field
-		if (0 < fil && fil < ydim && 0 < col && col < xdim) {
-			data[fil * xdim + col].dz = pml[col].gi3 * pml[fil].gj3 * data[fil * xdim + col].dz
-				+ pml[col].gi2 * pml[fil].gj2 * 0.5 * (data[fil * xdim + col].hy - data[fil  * xdim + col-1].hy
-					- data[fil * xdim + col].hx + data[(fil-1) * xdim + col ].hx);
-		}
-		
-		//Source
-		pulse = exp(-0.5 * ((t0 - time_step) / spread) * ((t0 - time_step) / spread));
-		field[3].ez_inc = pulse;
-		
-		//Incident Dz values
-		if (ia <= col && col < ib + 1) {
-			data[ja * xdim + col].dz = data[ja * xdim + col].dz + 0.5 * field[ja - 1].hx_inc;
-			data[jb * xdim + col].dz = data[jb * xdim + col].dz - 0.5 * field[jb].hx_inc;
-		}
-		
-		//Calculate the Ez field
-		if (fil < ydim && col < xdim) {
-			data[fil * xdim + col].ez = data[fil * xdim + col].gaz * (data[fil * xdim + col].dz - data[fil * xdim + col].iz);
-			data[fil * xdim + col].iz = data[fil * xdim + col].iz + data[fil * xdim + col].gbz * data[fil * xdim + col].ez;
-		}
-		
-		//Calculate the Incident Hx
-		if (fil < ydim - 1) {
-			field[fil].hx_inc = field[fil].hx_inc + 0.5 * (field[fil].ez_inc - field[fil + 1].ez_inc);
-		}
-		
-		//Calculate the Hx field
-		if(fil<ydim-1 && col<xdim-1){
-			data[fil * xdim + col].curl_e = data[fil * xdim + col].ez - data[(fil + 1) * xdim + col].ez;
-			data[fil * xdim + col].ihx = data[fil * xdim + col].ihx + data[fil * xdim + col].curl_e;
-			data[fil * xdim + col].hx = pml[fil].fj3 * data[fil * xdim + col].hx + pml[fil].fj2 *
-				(0.5 * data[fil * xdim + col].curl_e + pml[col].fi1 * data[fil * xdim + col].ihx);
-		}
-		
-		//Incident Hx values
-		if (ia < col && col < ib + 1) {
-			data[(ja-1) * xdim + col].hx = data[(ja - 1) * xdim + col].hx + 0.5 * field[ja].ez_inc;
-			data[jb * xdim + col].hx = data[jb * xdim + col].hx - 0.5 * field[jb].ez_inc;
-		}
-		
-		//Calculate the Hy field
-		if (col < xdim - 1 && fil < ydim) {
-			data[fil * xdim + col].curl_e = data[fil * xdim + col].ez - data[fil * xdim + col + 1].ez;
-			data[fil * xdim + col].ihy = data[fil * xdim + col].ihy + data[fil * xdim + col].curl_e;
-			data[fil * xdim + col].hy = pml[col].fi3 * data[fil * xdim + col].hy - pml[col].fi2 *
-				(0.5 * data[fil * xdim + col].curl_e + pml[fil].fj1 * data[fil * xdim + col].ihy);
-		}
-		
-		//Incident Hy values
-		if (ja < fil && fil < jb + 1) {
-			data[fil * xdim + ia - 1].hy = data[fil * xdim + ia - 1].hy - 0.5 * field[fil].ez_inc;
-			data[fil * xdim + ib].hy = data[fil * xdim + ib].hy + 0.5 * field[fil].ez_inc;
-		}
-		
+	//Incident Ez values
+	if (0 < fil && fil < ydim) {
+		data[fil * xdim + 1].aux = field[fil].ez_inc;
 	}
+	__syncthreads();
+	if (0 < fil && fil < ydim) {
+		field[fil].ez_inc = data[fil * xdim + 1].aux + 0.5 * (field[fil - 1].hx_inc - field[fil].hx_inc);
+	}
+	__syncthreads();
 
-	/*
+	//Absorbing Boundary Conditions
+	field[0].ez_inc = field[0].aux2;
+	field[0].aux2 = field[1].aux2;
+	field[1].aux2 = field[1].ez_inc;
+
+	field[ydim - 1].ez_inc = field[ydim - 1].aux2;
+	field[ydim - 1].aux2 = field[ydim - 2].aux2;
+	field[ydim - 2].aux2 = field[ydim - 2].ez_inc;
+
+	//Calculate the Dz field
+	if (fil < ydim && col < xdim) {
+		data[fil * xdim + col].aux = data[fil * xdim + col].dz;
+	}
+	__syncthreads();
+	if (0 < fil && fil < ydim && 0 < col && col < xdim) {
+		data[fil * xdim + col].dz = pml[col].gi3 * pml[fil].gj3 * data[fil * xdim + col].aux
+			+ pml[col].gi2 * pml[fil].gj2 * 0.5 * (data[fil * xdim + col].hy - data[fil * xdim + col - 1].hy
+				- data[fil * xdim + col].hx + data[(fil - 1) * xdim + col].hx);
+	}
+	__syncthreads();
+
+	//Source
+	pulse = exp(-0.5 * ((t0 - time_step) / spread) * ((t0 - time_step) / spread));
+	field[3].ez_inc = pulse;
+
+	//Incident Dz values
+	if (fil < ydim && col < xdim) {
+		data[fil * xdim + col].aux = data[fil * xdim + col].dz;
+	}
+	__syncthreads();
+	if (ia <= col && col < ib + 1) {
+		data[ja * xdim + col].dz = data[ja * xdim + col].aux + 0.5 * field[ja - 1].hx_inc;
+	}
+	__syncthreads();
+	if (ia <= col && col < ib + 1) {
+		data[jb * xdim + col].dz = data[jb * xdim + col].aux - 0.5 * field[jb].hx_inc;
+	}
+	__syncthreads();
+
+	//Calculate the Ez field
+	if (fil < ydim && col < xdim) {
+		data[fil * xdim + col].ez = data[fil * xdim + col].gaz * (data[fil * xdim + col].dz - data[fil * xdim + col].iz);
+		data[fil * xdim + col].aux = data[fil * xdim + col].iz;
+	}
+	__syncthreads();
+	if (fil < ydim && col < xdim) {
+		data[fil * xdim + col].iz = data[fil * xdim + col].aux + data[fil * xdim + col].gbz * data[fil * xdim + col].ez;
+	}
+	__syncthreads();
+
+}
+
+__global__ void FDTD2(FDTDData* data, PML* pml, INCFIELD* field, int xdim, int ydim, int time_step) {
+
 	int fil = blockIdx.y * blockDim.y + threadIdx.y;
 	int col = blockIdx.x * blockDim.x + threadIdx.x;
-	
-	if (fil<xdim && col<ydim) {
+	int ia = 7,
+		ib = xdim - ia - 1,
+		ja = 7,
+		jb = ydim - ja - 1;
 
-		data[fil * xdim + col].ez = 6;
+
+
+	//Calculate the Incident Hx
+	if (fil < ydim) {
+		data[fil * xdim + 1].aux = field[fil].hx_inc;
 	}
-	*/
-	
+	__syncthreads();
+	if (fil < ydim - 1) {
+		field[fil].hx_inc = data[fil * xdim + 1].aux + 0.5 * (field[fil].ez_inc - field[fil + 1].ez_inc);
+	}
+	__syncthreads();
+
+	//Calculate the Hx field
+	if (fil < ydim - 1 && col < xdim - 1) {
+		data[fil * xdim + col].curl_e = data[fil * xdim + col].ez - data[(fil + 1) * xdim + col].ez;
+		data[fil * xdim + col].aux = data[fil * xdim + col].ihx;
+	}
+	__syncthreads();
+	if (fil < ydim - 1 && col < xdim - 1) {
+		data[fil * xdim + col].ihx = data[fil * xdim + col].aux + data[fil * xdim + col].curl_e;
+		data[fil * xdim + col].aux = data[fil * xdim + col].hx;
+	}
+	__syncthreads();
+	if (fil < ydim - 1 && col < xdim - 1) {
+		data[fil * xdim + col].hx = pml[fil].fj3 * data[fil * xdim + col].aux + pml[fil].fj2 *
+			(0.5 * data[fil * xdim + col].curl_e + pml[col].fi1 * data[fil * xdim + col].ihx);
+	}
+	__syncthreads();
+
+
+	//Incident Hx values
+	if (fil < ydim && col < xdim) {
+		data[fil * xdim + col].aux = data[fil * xdim + col].hx;
+	}
+	__syncthreads();
+	if (ia <= col && col < ib + 1) {
+		data[(ja - 1) * xdim + col].hx = data[(ja - 1) * xdim + col].aux + 0.5 * field[ja].ez_inc;
+	}
+	__syncthreads();
+	if (ia <= col && col < ib + 1) {
+		data[jb * xdim + col].hx = data[jb * xdim + col].aux - 0.5 * field[jb].ez_inc;
+	}
+	__syncthreads();
+
+	//Calculate the Hy field
+	if (col < xdim - 1 && fil < ydim) {
+		data[fil * xdim + col].curl_e = data[fil * xdim + col].ez - data[fil * xdim + col + 1].ez;
+		data[fil * xdim + col].aux = data[fil * xdim + col].ihy;
+	}
+	__syncthreads();
+	if (col < xdim - 1 && fil < ydim) {
+		data[fil * xdim + col].ihy = data[fil * xdim + col].aux + data[fil * xdim + col].curl_e;
+		data[fil * xdim + col].aux = data[fil * xdim + col].hy;
+	}
+	__syncthreads();
+	if (col < xdim - 1 && fil < ydim) {
+		data[fil * xdim + col].hy = pml[col].fi3 * data[fil * xdim + col].aux - pml[col].fi2 *
+			(0.5 * data[fil * xdim + col].curl_e + pml[fil].fj1 * data[fil * xdim + col].ihy);
+	}
+	__syncthreads();
+
+	//Incident Hy values
+	if (fil < ydim && col < xdim) {
+		data[fil * xdim + col].aux = data[fil * xdim + col].hy;
+	}
+	__syncthreads();
+	if (ja <= fil && fil < jb + 1) {
+		data[fil * xdim + ia - 1].hy = data[fil * xdim + ia - 1].aux - 0.5 * field[fil].ez_inc;
+		data[fil * xdim + ib].hy = data[fil * xdim + ib].aux + 0.5 * field[fil].ez_inc;
+	}
+	__syncthreads();
+
+
 }
 
 int main(void)
@@ -289,12 +349,12 @@ int main(void)
 		INCFIELDbytes = dimx * INCFIELDsize,
 		PMLnumbytes = dimx * PMLsize,
 		datanumbytes = dimension * FDTDDatasize;
-	
+
 
 	//Create PML
-	PML* hst_pml;	
+	PML* hst_pml;
 	hst_pml = (PML*)malloc(PMLnumbytes);
-		
+
 	PMLGEN(hst_pml, dimx, dimy);
 
 	//Create the cylinder
@@ -305,7 +365,7 @@ int main(void)
 
 	//Initialization
 	INCFIELD* hst_field;
-	hst_field= (INCFIELD*)malloc(INCFIELDbytes);
+	hst_field = (INCFIELD*)malloc(INCFIELDbytes);
 
 	ini(hst_data, hst_field, dimx, dimy);
 
@@ -322,25 +382,59 @@ int main(void)
 	cudaMemcpy(dev_pml, hst_pml, PMLnumbytes, cudaMemcpyHostToDevice);
 	cudaMemcpy(dev_field, hst_field, INCFIELDbytes, cudaMemcpyHostToDevice);
 
-	dim3 blocks(2,2);
+	dim3 blocks(2, 2);
 	dim3 threads(25, 25);
+	int nsteps = 95;
 
-	//main loop
-	FDTD <<<blocks, threads >>> (dev_data, dev_pml, dev_field, dimx, dimy);
+	for (int ts = 1;ts < nsteps + 1;ts++)
+	{
+		//main loop
+		FDTD << <blocks, threads >> > (dev_data, dev_pml, dev_field, dimx, dimy, ts);
+		cudaDeviceSynchronize();
+		FDTD2 << <blocks, threads >> > (dev_data, dev_pml, dev_field, dimx, dimy, ts);
+		cudaDeviceSynchronize();
 
+	}
 	cudaMemcpy(hst_data, dev_data, datanumbytes, cudaMemcpyDeviceToHost);
 	cudaMemcpy(hst_pml, dev_pml, PMLnumbytes, cudaMemcpyDeviceToHost);
 	cudaMemcpy(hst_field, dev_field, INCFIELDbytes, cudaMemcpyDeviceToHost);
 
-	
+	/*
 	for (int i = 0;i < dimx; i++) {
-		for (int j = 0;j <dimy;j++) {
+		for (int j = 0;j < dimy;j++) {
 
-			cout <<setprecision(6) << hst_data[j * dimx + i].curl_e << ",  ";
+			//cout <<setprecision(12) << hst_data[i * dimx + j].gaz << ",  ";
+			if (hst_data[i * dimx + j].dz != 0) {
+				cout << i << "," << j<<";     ";
+			}
 		}
-		cout << endl;
+		//cout << setprecision(13) <<i<<":" << hst_field[i].ez_inc << endl;
 	}
-	
+	//cout << setprecision(17) << hst_data[14 * dimx + 36].ez << ",  ";
+	*/
+
+	//Matrix writing 
+	ofstream ez;
+	ez.open("ez.txt");
+
+	for (int i = 0;i < dimx; i++) {
+		for (int j = 0;j < dimy;j++) {
+
+			ez << setprecision(12) << hst_data[j * dimx + i].ez << ",  ";
+		}
+		ez << endl;
+	}
+
+	ez.close();
+
+	//Free memory
+	free(hst_data);
+	free(hst_pml);
+	free(hst_field);
+
+	cudaFree(dev_data);
+	cudaFree(dev_pml);
+	cudaFree(dev_field);
 
 	return 0;
 }
